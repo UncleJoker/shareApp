3dcb371da66f5266fc01d05acc98abbd1759844d
KeppRun <KeepRun@gmail.com>
2020-03-11 20:13:32 +0800

    更新波浪进度条

---
+++ GuideBook/Classes/Main/V/GBMainTableHeadView.h
@@ -0,0 +1,17 @@
+//
+//  GBMainTableHeadView.h
+//  GuideBook
+//
+//  Created by Apple on 2020/3/11.
+//  Copyright © 2020 Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GBMainTableHeadView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GuideBook/Classes/Main/V/GBMainTableHeadView.m
@@ -0,0 +1,35 @@
+//
+//  GBMainTableHeadView.m
+//  GuideBook
+//
+//  Created by Apple on 2020/3/11.
+//  Copyright © 2020 Design. All rights reserved.
+//
+
+#import "GBMainTableHeadView.h"
+
+@implementation GBMainTableHeadView
+
+- (instancetype)initWithFrame:(CGRect)frame{
+    if (self = [super initWithFrame:frame]) {
+        [self setView];
+    }
+    return self;
+}
+
+- (void)setView{
+    
+    
+    
+}
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
